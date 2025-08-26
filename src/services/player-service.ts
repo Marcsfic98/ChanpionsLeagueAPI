ed019c743c0f91c603802db215eb17b0c1a12920
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-08-26 09:15:27 -0300

    httpHelper criado

--- src/services/player-service.ts
+++ src/services/player-service.ts
@@ -1,5 +1,13 @@
+import { noContent, ok } from "../utils/http-helper"
 
 
 export const getPlayerService = async () => {
-    return {player:"Cr7Ronaldo"}
+    const data = {player:"Cr7Ronaldo"};
+    let response = null
+    if(data){
+         response = await  ok(data);
+    }else{
+        response = await  noContent();
+    }
+    return response;
 }
