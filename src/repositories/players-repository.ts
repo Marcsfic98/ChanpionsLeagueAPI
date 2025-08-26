ce4c5cf182ff608e6def6ff8ffffd5cc994d0094
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-08-26 09:25:22 -0300

    camada de repositories criada

---
+++ src/repositories/players-repository.ts
@@ -0,0 +1,17 @@
+interface playerModel{
+    id:number;
+    name:string;
+}
+
+const dataBase: playerModel[] = [
+    {id:1,name:"messi"},
+    {id:2,name:"Ronaldinho"}
+];
+
+export const findAllPlayers = async ():Promise<playerModel[]> => {
+    return dataBase
+};
+
+export const findPlayerById = async (id:number):Promise<playerModel | undefined> => {
+    return dataBase.find(player => player.id === id)
+}
